221f4633123b14209c4ef2ae63f45300212d06da
MoisesB27 <moisesdavidbatista@gmail.com>
2025-05-06 14:24:41 -0400

    Add files via upload
    mnm

---
+++ practica5.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practica_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 30; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
